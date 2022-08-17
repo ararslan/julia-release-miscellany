2e017fff206edcc27231671e2c3a9d1d23b8dd98
Alex Arslan <ararslan@comcast.net>
2022-08-17 16:40:55 -0700

    Check in script and notes for Kristoffer

---
+++ README.md
@@ -0,0 +1,139 @@
+# Odds and ends for Julia releases
+
+This repository is for notes, scripts, and whatever else I've found handy for making
+Julia releases over the years.
+Hopefully others who make releases will also find it handy.
+Note that the contents here reflect the state of play as of 2022-08-17, where 1.6 is LTS
+and 1.8.0 is coming out today, replacing 1.7 as stable.
+If you're reading this and you haven't seen any commits since then, note that what you're
+reading may be out of date.
+
+## The release process
+
+At a high level, these are the steps required to continue the release process after the
+PR that bumps the version is merged.
+
+1. Create and push the tag
+2. Ensure a _tag build_ was triggered on both buildkite and buildbot
+3. Build and GPG sign full and light source tarballs without BinaryBuilder dependencies
+4. Check for existence of all binaries in `s3://julialangnightlies` (naming is version-dependent)
+5. For earlier versions, e.g. 1.6, have Elliot notarize the macOS binaries
+5. Move binaries to `s3://julialang2`
+6. Compute and upload checksums
+7. Trigger a build of `versions.json`
+8. Update the website
+9. Announce on Discourse
+
+### Creating the tag
+
+```bash
+git pull
+git checkout release-1.x
+git tag v$(cat VERSION)
+git push origin --tags
+git rev-parse HEAD  # you may need this
+```
+
+The tag event should trigger builds on both buildkite and buildbot.
+
+### Build infrastructure wrangling
+
+Depending on the version, some platforms will be built on buildkite and some will be
+built on buildbot.
+For Julia 1.6, _everything_ is built on buildbot, but most platforms have since migrated
+to buildkite.
+Windows (x86 and x86\_64) and FreeBSD (x86\_64) are still always built on buildbot.
+
+Note that for 1.6, Elliot will need to notarize the macOS x86\_64 disk image file manually.
+This happens automatically on buildkite for both x86\_64 and AArch64.
+
+#### 1.6
+
+Most buildbots have been turned off so building for 1.6 requires asking Elliot to turn
+everything back on.
+The buildbot web interface may show large backlogs of pending builds for platforms that
+have migrated to buildkite; those can be safely canceled.
+
+Trigger a manual build for each platform using the SHA of the tag.
+Once the builds have completed, ask Elliot to turn the buildbots back off.
+Ensure that the corresponding binaries exist in `s3://julialangnightlies`.
+The naming scheme of the binaries is fully consistent for 1.6:
+
+```
+s3://julialangnightlies/pretesting/<os>/<arch>/1.6/julia-<hash>-<suffix>
+```
+
+Here, `hash` is the first 10 characters in the tag SHA and `suffix` is specific to the
+OS, architecture, and file type.
+These are handled by the script.
+
+#### Later versions
+
+The platforms on buildbot vs. buildkite are slightly different for 1.7 vs. 1.8+ but we'll
+ignore 1.7 here.
+We need only ensure that a tag build was properly captured on both buildkite and buildbot,
+otherwise the directory inside an unpacked tarball as well as the REPL banner will be
+incorrect.
+
+The naming scheme for _most_ platforms after 1.6 is:
+
+```
+s3://julialangnightlies/bin/<os>/<arch>/1.x/julia-<version>-<suffix>
+```
+
+where `version` is the tag name (without a leading `v`) and `suffix` matches that for 1.6.
+A better naming scheme is being put in place for recent versions but the old scheme is
+implemented alongside of it.
+Cases where the old scheme doesn't exist are technically bugs but they do exist.
+I believe an example of that is Linux AArch64.
+
+### Binary dance
+
+Moving binaries from the `julialangnightlies` bucket to `julialang2` is handled by the
+script, as is computing and uploading checksums.
+
+### Source tarballs
+
+We don't currently have a builder that produces source tarballs, so they must be made
+manually via
+
+```
+make full-source-dist light-source-dist USE_BINARYBUILDER=0
+```
+
+I usually do this on antarctic with many concurrent jobs (the `-j` flag for `make`).
+That command produces two tarballs in the root of the directory, the names of which must
+be adjusted (IIRC it duplicates the version in the name).
+The tarballs must then be GPG signed with the appropriate signing key:
+
+```
+gpg -u julia --armor --detach-sig julia-<version>.tar.gz
+gpg -u julia --armor --detach-sig julia-<version>-full.tar.gz
+```
+
+### Creating a GitHub release
+
+This is straightforward.
+Just create a release associated with the tag, name it the same as the tag version, write
+something in the description if you want, upload the source tarballs and .asc files as
+release artifacts.
+Check the prerelease box at the bottom for betas, RCs, etc. as applicable.
+
+### Rebuilding versions.json
+
+Go to <https://github.com/JuliaLang/VersionsJSONUtil.jl/actions/workflows/CI.yml> and
+click "Run workflow," ensure "Branch: main" is selected, then hit the button.
+This takes a bit under 2 hours to complete.
+
+### Updating the website
+
+Edit <https://github.com/JuliaLang/www.julialang.org/blob/main/config.md> to reflect the
+new version.
+Use the date as shown in the REPL banner as the release date, which should be the date the
+git tag was created.
+Ensure that the platforms listed in the corresponding table on the [downloads
+page](https://github.com/JuliaLang/www.julialang.org/blob/main/downloads/index.md)
+reflect the available binaries for this release.
+Add an entry to the [old releases
+page](https://github.com/JuliaLang/www.julialang.org/blob/main/downloads/oldreleases.md)
+if applicable.
---
+++ binaries.jl
@@ -0,0 +1,233 @@
+# I apologize in advance for this code. I usually just run it piecemeal in the REPL
+# and never quite bothered to polish it.
+
+using AWS
+using AWSS3
+using Base.Threads
+using MD5
+using Pkg.BinaryPlatforms
+using SHA
+
+global_aws_config(; profile="julia", region="us-east-1")
+
+version = v"1.8.0"
+
+macos_already_notarized = false  # whether Elliot notarized manually and put in julialang2
+
+destination = joinpath(homedir(), "Projects", "releases", string(version))
+
+commit = cd(joinpath(homedir(), "Projects", "julia")) do
+    run(`git fetch origin --tags --quiet`)
+    return readchomp(`git rev-parse v$version`)[1:10]
+end
+
+short_version = string(version.major, '.', version.minor)
+
+platforms = [
+    FreeBSD(:x86_64),
+    Linux(:x86_64),
+    Linux(:i686),
+    Linux(:aarch64),
+    Linux(:armv7l),
+    Linux(:ppc64le),
+    Linux(:x86_64; libc=:musl),
+    MacOS(:x86_64),
+    MacOS(:aarch64),
+    Windows(:x86_64),
+    Windows(:i686),
+]
+
+builder(::Union{FreeBSD,Windows}) = :buildbot
+if version >= v"1.8.0-"
+    builder(::Any) = :buildkite
+elseif version >= v"1.7.0-"
+    # I think this is true
+    builder(p::Linux) = libc(p) === :musl ? :buildbot : :buildkite
+    builder(::Any) = :buildkite
+else
+    builder(::Any) = :buildbot
+end
+
+short_name(::FreeBSD) = "freebsd"
+short_name(::Windows) = "winnt"
+short_name(::MacOS) = "mac"
+short_name(p::Linux) = libc(p) === :musl ? "musl" : "linux"
+
+function short_arch(p)
+    a = arch(p)
+    return if a === :powerpc64le
+        "ppc64le"
+    elseif a === :aarch64 || a === :armv7l
+        String(a)
+    elseif a === :x86_64
+        "x64"
+    elseif a === :i686
+        "x86"
+    end
+end
+
+exts(::Union{Linux,FreeBSD}) = ["tar.gz", "tar.gz.asc"]
+exts(::MacOS) = ["dmg", "tar.gz", "tar.gz.asc"]
+exts(::Windows) = ["exe", "zip", "tar.gz"]
+
+function nightly_name(platform, commit, version=version)
+    a = arch(platform)
+    suffix = if a === :x86_64 || a === :i686
+        string(wordsize(platform))
+    elseif a === :powerpc64le
+        "ppc64"
+    elseif builder(platform) === :buildkite && platform == Linux(:aarch64)
+        "-aarch64"
+    else
+        String(a)
+    end
+    os = short_name(platform)
+    os == "winnt" && (os = "win")
+    if v"1.6.0-" <= version < v"1.7.0-" || builder(platform) === :buildbot
+        return string("julia-", commit, '-', os, suffix)
+    else
+        return string("julia-", version, '-', os, suffix)
+    end
+end
+
+function nightly_url(platform, commit, ext, prefix="bin")
+    path = [prefix, short_name(platform), short_arch(platform),
+            short_version, nightly_name(platform, commit)]
+    return S3Path("julialangnightlies", join(path, '/') * '.' * ext)
+end
+
+function release_name(platform, version)
+    os = short_name(platform)
+    a = arch(platform)
+    suffix = if os == "winnt" || (os == "mac" && a !== :aarch64)
+        string(os[1:3], wordsize(platform))
+    elseif os == "mac" && a === :aarch64
+        string(os, a)
+    else
+        string(os, '-', a === :powerpc64le ? "ppc64le" : a)
+    end
+    return join(["julia", version, suffix], '-')
+end
+
+function release_url(platform, version, ext)
+    path = ["bin", short_name(platform), short_arch(platform),
+            short_version, release_name(platform, version)]
+    return S3Path("julialang2", join(path, '/') * '.' * ext)
+end
+
+ispath(destination) || mkpath(destination)
+for platform in platforms
+    for ext in exts(platform)
+        nightly = nightly_url(platform, commit, ext)
+        release = release_url(platform, version, ext)
+        @info platform ext nightly release
+        if !success(```
+                    aws s3api head-object
+                        --bucket $(nightly.bucket)
+                        --key $(join(nightly.segments, '/'))
+                        --no-paginate
+                        --no-cli-pager
+                        --profile julia
+                    ```)
+            @warn "Skipping $nightly, does not exist"
+            if !(Sys.isapple(platform) && macos_already_notarized)
+                continue
+            end
+        end
+        if !(Sys.isapple(platform) && macos_already_notarized && ext == "dmg")
+            @info "Copying from nightly to release"
+            # Don't inherit the source ACL to avoid having the release binaries publicly
+            # visible before they're guaranteed "final"
+            run(```
+                aws s3api copy-object
+                    --copy-source $(nightly.bucket)/$(join(nightly.segments, '/'))
+                    --bucket $(release.bucket)
+                    --key $(join(release.segments, '/'))
+                    --acl bucket-owner-full-control
+                    --no-paginate
+                    --no-cli-pager
+                    --profile julia
+                ```)
+        end
+        # Download locally for checksumming, but skip .asc
+        if !endswith(ext, ".asc")
+            @info "Downloading release locally"
+            if isfile(joinpath(destination, basename(release)))
+                @info "Already downloaded, skipping"
+                continue
+            end
+            try
+                run(```
+                    aws s3 cp
+                        $release
+                        $(joinpath(destination, basename(release)))
+                        --profile julia
+                    ```)
+            catch ex
+                @error "Oopsie poopsie" exception=(ex, catch_backtrace())
+            end
+        end
+    end
+end
+
+files = filter!(readdir(destination; join=true)) do file
+    isfile(file) || return false
+    file == ".DS_Store" && return false  # 😑
+    ext = last(splitext(file))
+    return !(ext in [".sha256", ".md5", ".asc"])
+end
+checksums = map(files) do file
+    bytes = read(file)
+    return (basename(file), bytes2hex(sha256(bytes)), bytes2hex(md5(bytes)))
+end
+sort!(checksums; by=first)
+for (i, ext) in enumerate(("sha256", "md5"))
+    fname = joinpath(destination, "julia-$version.$ext")
+    open(fname, "w") do io
+        for t in checksums
+            println(io, t[i + 1], "  ", t[1])
+        end
+    end
+    run(```
+        aws s3 cp
+            $fname
+            s3://julialang2/bin/checksums/
+            --profile julia
+            --acl public-read
+        ```)
+end
+
+for platform in platforms, ext in exts(platform)
+    release = release_url(platform, version, ext)
+    @info "Processing" release
+    if !success(```
+                aws s3api head-object
+                    --bucket $(release.bucket)
+                    --key $(join(release.segments, '/'))
+                    --no-paginate
+                    --no-cli-pager
+                    --profile julia
+                ```)
+        @warn "Skipping $release, does not exist"
+        continue
+    end
+    run(pipeline(```
+                 aws s3api put-object-acl
+                     --bucket $(release.bucket)
+                     --key $(join(release.segments, '/'))
+                     --acl public-read
+                     --profile julia
+                 ```; stdout=devnull))
+    parts = collect(release.segments)
+    parts[end] = replace(parts[end], string(version) => "$short_version-latest")
+    latest = join(parts, '/')
+    run(```
+        aws s3 cp
+            $release
+            s3://$(release.bucket)/$latest
+            --acl public-read
+            --profile julia
+        ```)
+    run(pipeline(`curl -s -X PURGE https://julialang-s3.julialang.org/$latest`;
+                 stdout=devnull))
+end
